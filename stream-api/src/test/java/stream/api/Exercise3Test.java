e97670d6d145d9b0b448a32501a9cd632138c2ad
SalahMswi <salah.almoussawi@viseo.com>
2016-08-23 00:51:04 +0200

    finish test 3 and 4

--- stream-api/src/test/java/stream/api/Exercise3Test.java
+++ stream-api/src/test/java/stream/api/Exercise3Test.java
@@ -24,7 +24,8 @@ public class Exercise3Test extends ClassicOnlineStore {
         /**
          * Count how many items there are in {@link Customer.wantToBuy} using {@link Stream#count}
          */
-        long sum = 0L;
+
+        long sum = customerList.stream().flatMap(customer -> customer.getWantToBuy().stream()).count();
 
         assertThat(sum, is(32L));
     }
@@ -38,9 +39,9 @@ public class Exercise3Test extends ClassicOnlineStore {
          * Find the richest customer's budget by using {@link Stream#max} and {@link Comparator#naturalOrder}
          * Don't use {@link Stream#sorted}
          */
-        Comparator<Integer> comparator = null;
-        Optional<Integer> richestCustomer = null;
-
+//        Optional<Integer> richestCustomer = customerList.stream().map();
+        Comparator<Integer> comparator = Comparator.naturalOrder();
+        Optional<Integer> richestCustomer = customerList.stream().map(c->c.getBudget()).max(comparator);
         assertThat(comparator.getClass().getSimpleName(), is("NaturalOrderComparator"));
         assertThat(richestCustomer.get(), is(12000));
     }
@@ -54,9 +55,8 @@ public class Exercise3Test extends ClassicOnlineStore {
          * Find the youngest customer by using {@link Stream#min}
          * Don't use {@link Stream#sorted}
          */
-        Comparator<Customer> comparator = null;
-        Optional<Customer> youngestCustomer = null;
-
+        Comparator<Customer> comparator = Comparator.comparing(cus->cus.getAge());
+        Optional<Customer> youngestCustomer = customerList.stream().min(comparator);
         assertThat(youngestCustomer.get(), is(customerList.get(8)));
     }
 }
