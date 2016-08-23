4652ea6fc677858a79b5cf1c1d27a6436fc62d1f
SalahMswi <salah.almoussawi@viseo.com>
2016-08-23 23:19:05 +0200

    finish test 7

--- stream-api/src/test/java/stream/api/Exercise6Test.java
+++ stream-api/src/test/java/stream/api/Exercise6Test.java
@@ -19,7 +19,7 @@ public class Exercise6Test {
         /**
          * Create a stream with string values "a" "b" "c" by using {@link Stream#of}
          */
-        
+
         Stream<String> abcStream = Stream.of("a","b","c");
 
         List<String> abcList = abcStream.collect(Collectors.toList());
